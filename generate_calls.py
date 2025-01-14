73c1d42c876538f52d1a57224e04fee1a4150016
cosminaRadus <rds.cosmina@yahoo.com>
2025-01-14 11:27:36 +0100

    more realistic repeated calls

--- generate_calls.py
+++ generate_calls.py
@@ -7,7 +7,7 @@ import json
 faker = Faker()
 
 def generate_repeated_calls():
-    return random.random() < 0.2  
+    return random.random() < 0.15  
 
 def is_odd_hour():
     hour = random.randint(1, 24)
@@ -22,7 +22,7 @@ def generate_call_data():
         "source_no": faker.phone_number(),
         "destination_number": faker.phone_number(),  
         "call_duration": random.randint(1, 1200),  
-        "start_time": time.time(), 
+        "start_time": time.time() - random.randint(1, 1000000), 
         "source_location": faker.city(),  
         "scam_flag": None
     }
@@ -41,7 +41,11 @@ def send_to_kafka():
         repeat_call = generate_repeated_calls()
 
         if repeat_call and last_call is not None:
+
             call_data = last_call.copy()
+            call_data["call_id"] = faker.uuid4()
+            call_data["start_time"] += random.randint(30, 300)
+            call_data["call_duration"] = max(1, call_data["call_duration"] + random.randint(-10, 10))
         else:
             call_data = generate_call_data()
         
@@ -50,7 +54,7 @@ def send_to_kafka():
             call_data["scam_flag"] = 1 if random.random() < 0.7 else 0
         else:
             call_data["scam_flag"] = 0 if random.random() < 0.8 else 1
-
+    
         last_call = call_data
 
         producer.send("calls_topic", call_data)
